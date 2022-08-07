a1db92d51367fcdc027c469c8e66dae1ec9aeb20
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-08-07 11:08:28 +0200

    initial commit

--- backend/src/config/db_configuration.ts
+++ backend/src/config/db_configuration.ts
@@ -5,8 +5,8 @@ const conn = mongoose.connect(process.env.DB_URL, {
     useUnifiedTopology: true, 
     useNewUrlParser: true
     
-}).then(() => console.log(`database connected successfully`.magenta))
-  .catch((error) => {
+}).then(() => console.log(`database connected successfully`))
+  .catch((error: any) => {
     console.log(error);
     process.exit(1);
   });
